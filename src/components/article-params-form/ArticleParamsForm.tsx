d4184bfb7162a2415116a9818050c01847d50147
MrTheSumraak <maew202139@gmail.com>
2025-09-25 23:34:24 +0300

    исправление замечаний

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useRef, useState } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
@@ -13,6 +13,7 @@ import {
 } from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { Select } from 'src/ui/select';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
 import { Separator } from 'src/ui/separator';
 import { Text } from 'src/ui/text';
 import styles from './ArticleParamsForm.module.scss';
@@ -23,20 +24,29 @@ type articlePropsState = {
 
 export const ArticleParamsForm = ({ setArticleState }: articlePropsState) => {
 	const [formsState, setFormState] = useState(defaultArticleState);
-	const [isOpen, setIsOpen] = useState(false);
-	const openAside = () => setIsOpen(!isOpen);
-	const handleFieldChange =
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const asideRef = useRef(null);
+
+	const toggleAsideMenu = () => setIsMenuOpen(!isMenuOpen);
+
+	useOutsideClickClose({
+		isOpen: isMenuOpen,
+		onChange: () => setIsMenuOpen(false),
+		rootRef: asideRef,
+	});
+
+	const updateFieldValue =
 		<K extends keyof ArticleStateType>(field: K) =>
 		(value: ArticleStateType[K]) => {
 			setFormState((prev) => ({ ...prev, [field]: value }));
 		};
 
-	const applyProperties = (ev: React.FormEvent) => {
+	const submitForm = (ev: React.FormEvent) => {
 		ev.preventDefault();
 		setArticleState(formsState);
 	};
 
-	const formReset = (ev: React.FormEvent) => {
+	const resetFormFields = (ev: React.FormEvent) => {
 		ev.preventDefault();
 		setArticleState(defaultArticleState);
 		setFormState(defaultArticleState);
@@ -44,9 +54,11 @@ export const ArticleParamsForm = ({ setArticleState }: articlePropsState) => {
 
 	return (
 		<>
-			<ArrowButton isOpen={isOpen} onClick={openAside} />
+			<ArrowButton isOpen={isMenuOpen} onClick={toggleAsideMenu} />
 			<aside
-				className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
+				className={`${styles.container} ${
+					isMenuOpen ? styles.container_open : ''
+				}`}
 				style={
 					{
 						'--font-family': formsState.fontFamilyOption.value,
@@ -55,11 +67,12 @@ export const ArticleParamsForm = ({ setArticleState }: articlePropsState) => {
 						'--container-width': formsState.contentWidth.value,
 						'--bg-color': formsState.backgroundColor.value,
 					} as CSSProperties
-				}>
+				}
+				ref={asideRef}>
 				<form
 					className={styles.form}
-					onSubmit={applyProperties}
-					onReset={formReset}>
+					onSubmit={submitForm}
+					onReset={resetFormFields}>
 					<Text uppercase={true} weight={800} size={31}>
 						Задайте параметры
 					</Text>
@@ -67,33 +80,33 @@ export const ArticleParamsForm = ({ setArticleState }: articlePropsState) => {
 						selected={formsState.fontFamilyOption}
 						options={fontFamilyOptions}
 						title='Шрифт'
-						onChange={handleFieldChange('fontFamilyOption')}
+						onChange={updateFieldValue('fontFamilyOption')}
 					/>
 					<RadioGroup
 						name='fontSizeOptions'
 						title='Размер шрифта'
 						options={fontSizeOptions}
 						selected={formsState.fontSizeOption}
-						onChange={handleFieldChange('fontSizeOption')}
+						onChange={updateFieldValue('fontSizeOption')}
 					/>
 					<Select
 						selected={formsState.fontColor}
 						options={fontColors}
 						title='Цвет шрифта'
-						onChange={handleFieldChange('fontColor')}
+						onChange={updateFieldValue('fontColor')}
 					/>
 					<Separator />
 					<Select
 						selected={formsState.backgroundColor}
 						options={backgroundColors}
 						title='Цвет фона'
-						onChange={handleFieldChange('backgroundColor')}
+						onChange={updateFieldValue('backgroundColor')}
 					/>
 					<Select
 						selected={formsState.contentWidth}
 						options={contentWidthArr}
 						title='Ширина контента'
-						onChange={handleFieldChange('contentWidth')}
+						onChange={updateFieldValue('contentWidth')}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
